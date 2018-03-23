18c13438f2352ebcb57f02b5463a679b061b2245
Cheryl H <cheryl.huber@my.csun.edu>
2018-03-22 17:18:04 -0700

    Added letters to the top

--- src/hangman/GameController.java
+++ src/hangman/GameController.java
@@ -12,6 +12,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
@@ -20,6 +21,7 @@ public class GameController {
 
 	private final ExecutorService executorService;
 	private final Game game;
+	private final int NUMLTRS = 26;
 	private ImageView lastImage;
 	
 	public GameController(Game game) {
@@ -37,12 +39,19 @@ public class GameController {
 	@FXML
 	private VBox board ;
 	@FXML
+	private HBox ltrBoard = new HBox(15);
+	@FXML
+	private HBox ltrBoard2 = new HBox(15);
+	@FXML
 	private Label statusLabel ;
 	@FXML
 	private Label enterALetterLabel ;
 	@FXML
 	private TextField textField ;
-
+	@FXML
+	private Label lblLtrs[] = new Label[NUMLTRS];
+	private char ltrs[] = new char [] {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+										'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 
 	public void resetHangman() {
 		System.out.println("Reset Hangman Visuals");
@@ -50,11 +59,23 @@ public class GameController {
 
 	public void initialize() throws IOException {
 		System.out.println("in initialize");
+		initLtrs();
 		drawHangman();
 		addTextBoxListener();
 		setUpStatusLabelBindings();
 	}
-
+	private void initLtrs(){
+		for(int i=0;i<NUMLTRS;i++){
+			lblLtrs[i]=new Label(Character.toString(ltrs[i]));
+			if(i<(NUMLTRS/2)) {
+				ltrBoard.getChildren().add(lblLtrs[i]);
+			}
+			else{
+				ltrBoard2.getChildren().add(lblLtrs[i]);
+			}
+			//lblLtrs[i].setText(Character.toString(ltrs[i]));
+		}
+	}
 	private void addTextBoxListener() {
 		textField.textProperty().addListener(new ChangeListener<String>() {
 			@Override
@@ -86,6 +107,9 @@ public class GameController {
 	}
 
 	private void drawHangman() {
+		ltrBoard.getChildren().addAll();
+		//board.getChildren().add(ltrBoard);
+		//board.getChildren().add(ltrBoard2);
     	if(lastImage!=null)
 			board.getChildren().remove(lastImage);
     	int tries = Game.getInstance().getTries();
@@ -110,6 +134,8 @@ public class GameController {
 		
 	@FXML 
 	private void newHangman() {
+		ltrBoard.getChildren().clear();
+		ltrBoard2.getChildren().clear();
 		board.getChildren().clear();
 		game.reset();
 	}
